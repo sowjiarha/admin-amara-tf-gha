75c6951aedc811b1c09fc2f10440a72483f0b372
sowjiarha <128574497+sowjiarha@users.noreply.github.com>
2023-05-05 22:16:46 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -40,4 +40,31 @@ resource "aws_s3_bucket_ownership_controls" "ownership" {
   bucket = aws_s3_bucket.bucket.id
   acl    = "public-read-write"
 }*/
-"s3.tf" 70L, 1781B                                                        19,5          Top
+  resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.ownership,
+    aws_s3_bucket_public_access_block.example,
+  ]
+
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "public-read"
+}
+resource "aws_s3_bucket_website_configuration" "website_conf" {
+  bucket = aws_s3_bucket.bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "error.html"
+  }
+}
