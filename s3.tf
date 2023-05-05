fdbaa86117ba902454fd3ab4c7e33d175884cb4b
sowjiarha <128574497+sowjiarha@users.noreply.github.com>
2023-05-05 22:08:12 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -3,26 +3,30 @@ resource "aws_s3_bucket" "bucket" {
   bucket = "${var.bucketName}"
 }
 
-/*resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
   bucket = aws_s3_bucket.bucket.id
   policy = data.aws_iam_policy_document.allow_access_from_another_account.json
 }
 data "aws_iam_policy_document" "allow_access_from_another_account" {
   statement {
     principals {
-      type        = "AWS"
-      identifiers = ["*"]
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
     }
     actions = [
       "s3:GetObject",
       "s3:ListBucket"
     ]
     resources = [
-      aws_s3_bucket.bucket.arn,
       "${aws_s3_bucket.bucket.arn}/*",
     ]
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.www_distribution.arn]
   }
-}*/
+}
+}
 resource "aws_s3_bucket_ownership_controls" "ownership" {
   bucket = aws_s3_bucket.bucket.id
   rule {
@@ -30,19 +34,10 @@ resource "aws_s3_bucket_ownership_controls" "ownership" {
   }
 }
 
-resource "aws_s3_bucket_acl" "bucket_acl" {
+/*resource "aws_s3_bucket_acl" "bucket_acl" {
   depends_on = [aws_s3_bucket_ownership_controls.ownership]
 
   bucket = aws_s3_bucket.bucket.id
-  acl    = "private"
-}
-resource "aws_s3_bucket_website_configuration" "website_conf" {
-  bucket = aws_s3_bucket.bucket.id
-
-  index_document {
-    suffix = "index.html"
-  }
-  error_document {
-    key = "error.html"
-  }
-}
+  acl    = "public-read-write"
+}*/
+"s3.tf" 70L, 1781B                                                        19,5          Top
