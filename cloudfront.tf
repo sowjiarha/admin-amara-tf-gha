3ba091b63549677b9d1fc2752844c8850d0270b5
sowjiarha <128574497+sowjiarha@users.noreply.github.com>
2023-05-07 23:07:58 +0530

    Update cloudfront.tf

--- cloudfront.tf
+++ cloudfront.tf
@@ -56,5 +56,5 @@ resource "aws_cloudfront_origin_access_control" "arha-dev" {
   origin_access_control_origin_type = "s3"
   signing_behavior                  = "always"
   signing_protocol                  = "sigv4"
-}
+  }
                                                           
