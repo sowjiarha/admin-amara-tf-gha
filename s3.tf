2d677d53342b4cf0fa0fc6b7011610dc94a09e76
sowjiarha <128574497+sowjiarha@users.noreply.github.com>
2023-05-07 23:04:55 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,9 +1,10 @@
 resource "aws_s3_bucket" "bucket" {
   // Our bucket's name is going to be the same as our site's domain name
   bucket = "${var.bucketName}"
+  acl = "private"
 }
 
-resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+/*resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
   bucket = aws_s3_bucket.bucket.id
   policy = data.aws_iam_policy_document.allow_access_from_another_account.json
 }
@@ -15,56 +16,59 @@ data "aws_iam_policy_document" "allow_access_from_another_account" {
     }
     actions = [
       "s3:GetObject",
-      "s3:ListBucket"
+      "s3:ListBucket",
+      "s3:PutObject"
     ]
     resources = [
-      "${aws_s3_bucket.bucket.arn}/*",
+      "${aws_s3_bucket.bucket.arn}",
+      "${aws_s3_bucket.bucket.arn}/*"
     ]
     condition {
       test     = "StringEquals"
-      variable = "AWS:SourceArn"
-      values   = [aws_cloudfront_distribution.www_distribution.arn]
+      variables = {
+        "aws:Referer": "https://${aws_cloudfront_distribution.www_distribution.domain_name}"
+      }
+      #variable = "AWS:SourceArn"
+      #values   = [aws_cloudfront_distribution.www_distribution.arn]
   }
 }
-}
-resource "aws_s3_bucket_ownership_controls" "ownership" {
-  bucket = aws_s3_bucket.bucket.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-/*resource "aws_s3_bucket_acl" "bucket_acl" {
-  depends_on = [aws_s3_bucket_ownership_controls.ownership]
-
-  bucket = aws_s3_bucket.bucket.id
-  acl    = "public-read-write"
 }*/
-  resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.bucket.id
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.bucket.arn}/*"]
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn]
+    }
+  }
 }
 
-resource "aws_s3_bucket_acl" "example" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.ownership,
-    aws_s3_bucket_public_access_block.example,
-  ]
-
+resource "aws_s3_bucket_policy" "mybucket" {
   bucket = aws_s3_bucket.bucket.id
-  acl    = "public-read"
+  policy = data.aws_iam_policy_document.s3_policy.json
 }
+
+
 resource "aws_s3_bucket_website_configuration" "website_conf" {
   bucket = aws_s3_bucket.bucket.id
 
   index_document {
     suffix = "index.html"
-  }
+     }
   error_document {
     key = "error.html"
   }
 }
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "s3-my-webapp.example.com"
+ }
+resource "aws_s3_bucket_public_access_block" "mybucket" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  //ignore_public_acls      = true
+  //restrict_public_buckets = true
+}                                     
